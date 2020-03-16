ec7ce52cc61ed5dc57dad971c5ff79de81764f66
Paul Hutchings <paul@xps>
2020-03-16 13:04:17 -0400

    Changed config and test suite structure for multiple schools

--- test/banner.js
+++ test/banner.js
@@ -2,28 +2,41 @@
 
 var assert = require('assert');
 var Banner = require('../index');
+const config = require('../lib/config');
 
+/**
+ * Universal tests
+ */
 describe('Banner', function () {
   /**
-   * SETUP
-   */
-  const school = 'temple';
-  const term = 202003;
-  var cache = {};
-
-  /**
-   * CONSTRUCTOR
-   */
+     * CONSTRUCTOR
+     */
   describe('#constructor(school)', function () {
     it('Should throw an error when a school is not passed', function () {
-      assert.throws(() => new Banner(), Error, 'Must provide school');
+      assert.throws(function () { new Banner() }, Error, 'Must provide school');
+    });
+
+    it('Should throw an error when the school passed is not in the config', function () {
+      let school = 'lolz';
+      assert.throws(function () { new Banner(school) }, Error, `Unsupported school "${school}"`);
     });
   });
+});
 
-  /**
-   * SETUP
-   */
-  var banner = new Banner(school);
+Object.keys(config.schools).forEach(function (school) {
+  describe(`Banner-${school}`, function () {
+    /**
+     * SETUP
+     */
+    var cache = {};
 
-  
+
+
+    /**
+     * SETUP
+     */
+    var banner = new Banner(school);
+
+
+  });
 });
